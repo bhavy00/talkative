444dc39b2c7f98a03871c0ad9607b862ee1cd87e
Dogeshvar <dogeshvar@pop-os.localdomain>
2023-06-05 00:43:43 +0530

    databse connection files/code added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
 const app = require('./app')
+const connectDB = require('./db/connect')
 
-const PORT = 5000
+dotenv.config({ path: "./config/config.env" });
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on ${PORT}`);
-})
+const db = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        await connectDB(db);
+        app.listen(PORT, ()=>{
+            console.log(`Listening on ${PORT}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
